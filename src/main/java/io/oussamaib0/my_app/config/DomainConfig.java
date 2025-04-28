d482b76206fe04a8bec1e8eaef0125c937cd0925
Oussama-El-Amrani <oussama.elamrani4@usmba.ac.ma>
2025-04-28 09:24:54 +0100

    Initial commit

---
+++ src/main/java/io/oussamaib0/my_app/config/DomainConfig.java
@@ -0,0 +1,14 @@
+package io.oussamaib0.my_app.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Configuration
+@EntityScan("io.oussamaib0.my_app.domain")
+@EnableJpaRepositories("io.oussamaib0.my_app.repos")
+@EnableTransactionManagement
+public class DomainConfig {
+}
