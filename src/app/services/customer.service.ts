c9a140cb3e330a13cae13a5e6aa2fc549bdf54fd
Oussama-El-Amrani <oussama.elamrani4@usmba.ac.ma>
2025-05-05 09:45:00 +0100

    feat: Add legacy app module structure

---
+++ src/app/services/customer.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Customer } from '../model/customer.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+  private apiUrl = 'http://localhost:8080/api/customers';
+
+  constructor(private http: HttpClient) { }
+
+  getCustomers(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(this.apiUrl);
+  }
+  
+  // Add other methods as needed
+}
