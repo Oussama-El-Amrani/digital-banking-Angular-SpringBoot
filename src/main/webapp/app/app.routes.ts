8ed53af1ab0109009edadbe80f21460edd48d4f8
Oussama-El-Amrani <oussama.elamrani4@usmba.ac.ma>
2025-05-19 00:26:55 +0100

    feat: Configure application routing and main component structure

--- src/main/webapp/app/app.routes.ts
+++ src/main/webapp/app/app.routes.ts
@@ -1,13 +1,59 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
-
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+import { CustomerFormComponent } from './customer/customer-form/customer-form.component';
+import { CustomerDetailsComponent } from './customer/customer-details/customer-details.component';
+import { AccountFormComponent } from './account/account-form/account-form.component';
+import { AccountOperationsComponent } from './account/account-operations/account-operations.component';
+import { BankingDashboardComponent } from './banking-dashboard/banking-dashboard.component';
+import { ApiDocsComponent } from './api-docs/api-docs.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: $localize`:@@home.index.headline:Welcome to your new app!`
+  },
+  {
+    path: 'banking',
+    component: BankingDashboardComponent,
+    title: 'Digital Banking Dashboard'
+  },
+  {
+    path: 'customers',
+    component: CustomerListComponent,
+    title: 'Customers'
+  },
+  {
+    path: 'customers/new',
+    component: CustomerFormComponent,
+    title: 'Add New Customer'
+  },
+  {
+    path: 'customers/:id',
+    component: CustomerDetailsComponent,
+    title: 'Customer Details'
+  },
+  {
+    path: 'customers/:id/edit',
+    component: CustomerFormComponent,
+    title: 'Edit Customer'
+  },
+  {
+    path: 'accounts/new',
+    component: AccountFormComponent,
+    title: 'Create New Account'
+  },
+  {
+    path: 'accounts/:id/operations',
+    component: AccountOperationsComponent,
+    title: 'Account Operations'
+  },
+  {
+    path: 'api-docs',
+    component: ApiDocsComponent,
+    title: 'API Documentation'
   },
   {
     path: 'error',
