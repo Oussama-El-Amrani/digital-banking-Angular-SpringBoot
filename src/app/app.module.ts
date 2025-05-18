34dc7fbfbd3cffd36ce271b8fd09ebf81cc11ec0
Oussama-El-Amrani <oussama.elamrani4@usmba.ac.ma>
2025-05-19 00:30:07 +0100

    feat: Add legacy app module structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// Import your components here
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // Declare your components here
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
